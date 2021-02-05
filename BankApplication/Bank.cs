26e8284de9df6a7899ed6aef2ea23905bce20652
Sanchit Rastogi <8958698863sr@gmail.com>
2021-02-05 15:37:23 +0530

    added menu options and bank registration

---
+++ BankApplication/Bank.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace BankApplication
+{
+    public class Bank
+    {
+        public String Name;
+        public String DefaultCurrency;
+        public int SameBankRTGSCharge;
+        public int SameBankIMPSCharge;
+        public int DifferentBankRTGSCharge;
+        public int DifferentBankIMPSCharge;
+
+        public Bank() { }
+
+        public Bank(String name)
+        {
+            Name = name;
+            DefaultCurrency = "INR";
+            SameBankRTGSCharge = 0;
+            SameBankIMPSCharge = 5;
+            DifferentBankRTGSCharge = 2;
+            DifferentBankIMPSCharge = 6;
+        }
+
+        public void RegisterBank()
+        {
+            Console.WriteLine("Enter your bank name");
+            String name = Console.ReadLine();
+            Bank obj = new Bank(name);
+            UserFlow flow = new UserFlow();
+            Console.WriteLine("Bank with the name " + name + " is successfully registered !");
+            flow.DisplayMainMenu();
+        }
+    }
+}
---
+++ BankApplication/UserFlow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace BankApplication
+{
+    public class UserFlow
+    {
+        public UserFlow() { }
+
+        public void DisplayMainMenu()
+        {
+            Console.WriteLine("Welcome to The Bank Application Project");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Please enter option from the list :- ");
+            Console.WriteLine("");
+            Console.WriteLine("1. Create a Bank");
+            Console.WriteLine("2. Login for Staff / Account holder");
+            Console.WriteLine("3. Save and Exit");
+            int res = Convert.ToInt32(Console.ReadLine());
+            Bank bank = new Bank();
+            switch (res)
+            {
+                case 1:
+                    Console.WriteLine("Registereing a new bank");
+                    bank.RegisterBank();
+                    break;
+                case 2:
+                    Console.WriteLine("Option 2 is under work !");
+                    break;
+                case 3:
+                    Console.WriteLine("Option 3 is under work !");
+                    break;
+                default:
+                    Console.WriteLine("Please select a valid option !!");
+                    DisplayMainMenu();
+                    break;
+            }
+        }
+    }
+}
