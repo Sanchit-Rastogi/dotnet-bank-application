48ad5fa3cbdcc4b2b84801bd1c05346fd0288def
Sanchit Rastogi <8958698863sr@gmail.com>
2021-02-05 12:35:17 +0530

    initial commit

---
+++ BankApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to The Bank Application Project");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Please enter option from the list :- ");
+            Console.WriteLine("");
+            Console.WriteLine("1. Create a Bank");
+            Console.WriteLine("2. Login for staff/account holder");
+            Console.WriteLine("3. Save and Exit");
+        }
+    }
+}
