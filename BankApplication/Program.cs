63c60a786c2163fc78ee707ca6a3aeaac3cf10d4
Sanchit Rastogi <8958698863sr@gmail.com>
2021-03-03 11:50:35 +0530

    reading from xml file

--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace BankApplication
 {
@@ -7,6 +9,10 @@ namespace BankApplication
         static void Main(string[] args)
         {
             Bank bank = new Bank();
+            XmlSerializer serializer = new XmlSerializer(typeof(Bank));
+            FileStream fs = new FileStream("bankData.xml", FileMode.Open);
+            bank = (Bank)serializer.Deserialize(fs);
+            Console.WriteLine("Name of the saved bank is : " + bank.Name);
             BankApplication bankApp = new BankApplication(bank);
             bankApp.DisplayMainMenu();
         }
